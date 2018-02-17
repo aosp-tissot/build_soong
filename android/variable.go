dc1decc3f8a972dd54f87d78b709a04971760203
Paul Keith <javelinanddart@gmail.com>
2018-02-17 12:19:52 -0300

    variables: Add TARGET_LD_SHIM_LIBS variable handling

--- android/variable.go
+++ android/variable.go
@@ -99,6 +99,10 @@ type variableProperties struct {
 			Cppflags []string
 		}
 
+		Target_shim_libs struct {
+			Cppflags []string
+		}
+
 		Uses_generic_camera_parameter_library struct {
 			Srcs []string
 		}
@@ -182,6 +186,7 @@ type productVariables struct {
 
 	Needs_text_relocations *bool `json:",omitempty"`
 	Specific_camera_parameter_library *string `json:",omitempty"`
+	Target_shim_libs *string `json:",omitempty"`
 	Uses_generic_camera_parameter_library *bool `json:",omitempty"`
 	Uses_qcom_bsp_legacy *bool `json:",omitempty"`
 }
